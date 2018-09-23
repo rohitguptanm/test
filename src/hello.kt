ebcd5266e4bf3ebe5a9f92745a801a2f3ab4eb45
Rohit <rohitguptanm@gmail.com>
2018-09-23 19:16:26 +0530

    new text added after branch cration

--- src/hello.kt
+++ src/hello.kt
@@ -2,6 +2,7 @@ fun main(args:Array<String>){
     val a ="Hello"
     if(a)
     println("Hello from test")
+    println("new changes in master branch after creating user_branch1")
 
     println("hi")
     println("hello")
